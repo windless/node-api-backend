4a943a31e2acfee0b433133f1949e860943fa564
Abner Zhong <windlessg@gmail.com>
2017-03-07 17:19:32 +0800

    add bunyan

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import bunyan from 'bunyan'
+
+const log = bunyan.createLogger({name: 'app'})
 
 const app = express()
 
 app.listen(4000, () => {
-  console.log('Running on http://localhost:4000')
+  log.info('Running on http://localhost:4000')
 })
